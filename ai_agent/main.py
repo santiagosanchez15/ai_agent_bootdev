9fa7e7578914e164468d4fe7f691548675e6bb2c
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-05-01 15:00:27 -0700

    genai methods from google added

--- ai_agent/main.py
+++ ai_agent/main.py
@@ -1,3 +1,15 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if api_key is None: raise RuntimeError("API key not foound")
+client = genai.Client(api_key=api_key)
+gemini = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(gemini.text)
+
 def main():
     print("Hello from ai-agent!")
 
