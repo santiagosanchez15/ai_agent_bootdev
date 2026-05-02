22a2d1634baad11214102c6c491efbaab9e942fb
santiagosanchez15 <sanchezsantiago2004@gmail.com>
2026-05-02 14:05:36 -0700

    hardcoded gemini input for different responses

--- ai_agent/main.py
+++ ai_agent/main.py
@@ -7,12 +7,18 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None: raise RuntimeError("API key not foound")
 client = genai.Client(api_key=api_key)
-gemini = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
-print(gemini.text)
+
 
 def main():
-    print("Hello from ai-agent!")
 
+    gemini = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    
+    print(f"Prompt tokens: {gemini.usage_metadata.prompt_token_count}") # get number of token from given question
 
+    if gemini.usage_metadata.candidates_token_count is None: raise RuntimeError("no resposnse given by model") #get number of tokens by given models response
+    print(f"Response tokens: {gemini.usage_metadata.candidates_token_count}")
+
+    print(f"Response:\n {gemini.text}")
+    
 if __name__ == "__main__":
     main()
